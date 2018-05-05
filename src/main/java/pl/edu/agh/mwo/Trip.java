caa0b32db6321f092116fe37cb9cdb427039db46
robertbenko <robert.benko@gmail.com>
2018-05-05 12:23:40 +0200

    TripTest storing photos

--- src/main/java/pl/edu/agh/mwo/Trip.java
+++ src/main/java/pl/edu/agh/mwo/Trip.java
@@ -1,5 +1,23 @@
 package pl.edu.agh.mwo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Trip {
+
+    public String name;
+    public String description;
+    private List<Photo> photos = new ArrayList<>();
+
+    public void addPhoto(Photo p) {
+        if (p == null) {
+            throw new IllegalArgumentException("photo must not be null");
+        }
+        photos.add(p);
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
 
 }
---
+++ src/test/java/pl/edu/agh/mwo/TripTest.java
@@ -0,0 +1,33 @@
+package pl.edu.agh.mwo;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TripTest {
+
+    @Test
+    public void storingPhotosDefaultPath() {
+        // given
+        Trip trip = new Trip();
+        Photo photo1 = new Photo();
+        Photo photo2 = new Photo();
+        Photo photo3 = new Photo();
+
+        // when
+        trip.addPhoto(photo1);
+        trip.addPhoto(photo2);
+
+        // then
+        List<Photo> storedPhotos = trip.getPhotos();
+
+        assertEquals(2, storedPhotos.size());
+        assertTrue(storedPhotos.contains(photo1));
+        assertTrue(storedPhotos.contains(photo2));
+        assertFalse(storedPhotos.contains(photo3));
+    }
+}
