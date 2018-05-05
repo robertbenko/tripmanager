3229be1031f1f5aa4e440ed8076c654b555aee58
robertbenko <robert.benko@gmail.com>
2018-05-05 23:24:36 +0200

    TripManager add and get Trips

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -1,5 +1,18 @@
 package pl.edu.agh.mwo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TripManager {
+	
+	private List<Trip> trips = new ArrayList<>();
+	
+	public void addTrip(Trip trip) {
+		trips.add(trip);
+	}
+	
+	public List<Trip> getTrips() {
+		return trips;
+	}
 
 }
--- src/test/java/pl/edu/agh/mwo/TripManagerTest.java
+++ src/test/java/pl/edu/agh/mwo/TripManagerTest.java
@@ -1,5 +1,33 @@
 package pl.edu.agh.mwo;
- 
-public class TripManagerTest {
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class TripManagerTest {
+	
+	@Test
+	public void storingTripsDefaultPath() {
+		//given
+		TripManager tm = new TripManager();
+		
+		//when
+		Trip trip1 = new Trip();
+		Trip trip2 = new Trip();
+		//Trip trip3 = new Trip();
+		
+		tm.addTrip(trip1);
+		tm.addTrip(trip2);
+		
+		//then
+		List<Trip> storedTrips = tm.getTrips();
+		
+		assertEquals(2, storedTrips.size());
+		assertTrue(storedTrips.contains(trip1));
+		assertTrue(storedTrips.contains(trip2));
+	}
+	
 }
