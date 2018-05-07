c6e1fe339de2119034f1d78fff8e66cc142f0928
robertbenko <robert.benko@gmail.com>
2018-05-08 00:02:10 +0200

    findTrip implementation

--- src/main/java/pl/edu/agh/mwo/TripManager.java
+++ src/main/java/pl/edu/agh/mwo/TripManager.java
@@ -4,22 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TripManager {
-	
-	private List<Trip> trips = new ArrayList<>();
-	
-	public void addTrip(Trip trip) {
-		trips.add(trip);
-	}
-	
-	public List<Trip> getTrips() {
-		return trips;
-	}
-	
-	public void removeTrip(Trip trip) {
-		trips.remove(trip);
-	}
+    
+    private List<Trip> trips = new ArrayList<>();
+    private String name;
+    
+    public void addTrip(Trip trip) {
+        trips.add(trip);
+    }
+    
+    public List<Trip> getTrips() {
+        return trips;
+    }
+    
+    public void removeTrip(Trip trip) {
+        trips.remove(trip);
+    }
 
-	public Trip findTrip(String keyword) {
-		return null; //todo: implement this!
-	}
+    public Trip findTrip(String keyword) {
+        
+        for(Trip trip: trips) {
+            
+            if (trip.name != null && trip.name.contains(keyword))
+            	return trip;
+            
+        }
+        return null;
+    }
 }
